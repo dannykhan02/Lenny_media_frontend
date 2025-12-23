7d38ea96353784cfb61f92dbd3e329aca4ec4c97
dannykhan02 <dannykhan614@gmail.com>
2025-12-23 20:41:01 +0300

    fix: allow dist folder for Netlify build

---
+++ src/dist/assets/search-BnyDWve6.js
@@ -0,0 +1,11 @@
+import{c}from"./index-Cx_oq0n8.js";/**
+ * @license lucide-react v0.561.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],t=c("check",e);/**
+ * @license lucide-react v0.561.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"m21 21-4.34-4.34",key:"14j7rj"}],["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}]],a=c("search",o);export{t as C,a as S};
